7bbb8b08904de52a2240f3737b1344d7716c3ede
carlosperales95 <carlos.perales95@opendeusto.es>
2022-10-31 22:06:11 +0100

    First commit concept splashpage

---
+++ vue-padel-tour/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="base-container">
+    <!-- <app-header /> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+  components: {
+    // AppHeader
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #fff;
+}
+
+.base-container {
+  color: #fff;
+  background-color: #14251C;
+}
+
+</style>
---
+++ vue-padel-tour/src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <router-link to="/">
+      <img alt="Vue logo" src="../assets/logo.png" height="50">
+    </router-link>
+    <p>EspaÑish Padel Tournament</p>
+    <nav>
+      <ul>
+        <li><router-link to="/info">El torneo</router-link></li>
+        <li><router-link to="/register">Apuntate</router-link></li>
+        <li><router-link to="/about">Sobre Nosotros</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+  header {
+    display: flex;
+    border-bottom: 1px solid #ccc;
+    padding: .5rem 1rem;
+
+    p {
+      margin-left: 1rem;
+    }
+  }
+
+  a {
+    color: #ccc;
+    text-decoration: none;
+    font-size: 1.5em;
+    font-weight: 600;
+
+    &:hover{
+
+      color: aqua;
+
+    }
+  }
+
+  nav {
+    margin-left: auto;
+
+    ul {
+      list-style: none;
+    }
+
+    ul li {
+      display: inline-flex;
+      margin-left: 1rem;
+    }
+  }
+</style>
